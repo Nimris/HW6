29c8fb618281250d7a7090735e44b5c0738e9446
RavensLord <fenixmnio@gmail.com>
2024-09-22 15:52:01 +0300

    22.09

--- insert.py
+++ insert.py
@@ -5,34 +5,27 @@ from psycopg2 import DatabaseError
 
 from connect import create_connection
 
-
 fake = Faker('uk-UA')
-subjects = [("Math",), ("Physics",), ("Chemistry",), ("Biology",), ("History",), ("English",), ("Ukrainian",)]
-groups = [("A",), ("B",), ("C",)]
-
+subjects = ["Math", "Physics", "Chemistry", "Biology", "History", "English", "Ukrainian"]
+groups = ["A", "B", "C"]
 
 def insert_data(conn):
     with conn.cursor() as c:
         try:
-            for group in groups:
-                c.execute("INSERT INTO groups (name) VALUES (%s);", group)
-            conn.commit()
+            c.executemany("INSERT INTO groups (name) VALUES (%s);", [(group,) for group in groups])
             
-            for _ in range(40):
-                c.execute("INSERT INTO students (fullname, group_id) VALUES (%s, %s);", (fake.name(), randint(1, 3)))
-            conn.commit()
+            students = [(fake.name(), randint(1, len(groups))) for _ in range(40)]
+            c.executemany("INSERT INTO students (fullname, group_id) VALUES (%s, %s);", students)
+            
+            teachers = [(fake.name(),) for _ in range(4)]
+            c.executemany("INSERT INTO teachers (fullname) VALUES (%s);", teachers)
 
-            for _ in range(4):
-                c.execute("INSERT INTO teachers (fullname) VALUES (%s);", (fake.name(),))
-            conn.commit()    
-                        
-            for subject in subjects:
-                c.execute("INSERT INTO subjects (name, teacher_id) VALUES (%s, %s);", (subject, randint(1, 4)))
-            conn.commit()
-                
-            for _ in range(400):
-                c.execute("INSERT INTO grades (student_id, subject_id, grade, grade_date) VALUES (%s, %s, %s, %s);",
-                          (randint(1, 40), randint(1, 7), randint(1, 100), fake.date_this_year()))
+            subjects_data = [(subject, randint(1, 4)) for subject in subjects]
+            c.executemany("INSERT INTO subjects (name, teacher_id) VALUES (%s, %s);", subjects_data)
+            
+            grades = [(randint(1, 40), randint(1, len(subjects)), randint(1, 100), fake.date_this_year()) for _ in range(400)]
+            c.executemany("INSERT INTO grades (student_id, subject_id, grade, grade_date) VALUES (%s, %s, %s, %s);", grades)
+
             conn.commit()
             
         except DatabaseError as e:
@@ -40,7 +33,6 @@ def insert_data(conn):
             conn.rollback()
         finally:
             logging.info("Data insertion completed.")
-
 
 if __name__ == '__main__':
     try:
